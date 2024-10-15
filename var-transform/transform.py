b0324da4d5e296be2e89d2e2ef1ecbe71ddaf854
TakumiHiraoka <hi.ta9.23.333@gmail.com>
2024-10-15 23:01:06 +0900

    impl for logsumexp

--- var-transform/transform.py
+++ var-transform/transform.py
@@ -1,13 +1,18 @@
 import numpy as np
 from scipy import interpolate
-from utils import spline1, inv_calc, diff_calc
+from utils import spline_eq, spline_func, inv_calc, diff_calc, compress_range, calc_prob, visualize, plt_sca
 from noise_alg import laplace_func
+import matplotlib.pyplot as plt
 
 """
 変数変換の関数
-X_val: 変換前の確率密度関数の範囲を表す配列
-X_pdf_val: 変換前の確率密度関数の値の配列
-transform_func: 変換のための関数
+Args:
+    X_val: 変換前の確率密度関数の範囲を表す一次元配列
+    X_pdf_val: 変換前の確率密度関数の値の一次元配列
+    transform_func: 変換のための関数
+Return:
+    変換後の確率密度関数の範囲を表す一次元配列 (等間隔でなくて良い。無理に等間隔にすることで関数にとって重要な情報が損なわれてしまう)
+    変換後の確率密度関数の値の一次元配列
 """
 def trasform_var(X_val: np.ndarray, X_pdf_val: np.ndarray, transform_func) -> tuple[np.ndarray, np.ndarray]:
     assert X_val.size == X_pdf_val.size
@@ -15,17 +20,39 @@ def trasform_var(X_val: np.ndarray, X_pdf_val: np.ndarray, transform_func) -> tu
     # 逆関数を数値的に計算する
     x_inv, y_inv = inv_calc(X_val, Y_val)
     # スプライン補間の際のデータ点の数
-    point_num = X_val.size * 10
-    x_splined, y_splined = spline1(x_inv, y_inv, point_num)
-    dx_dy = diff_calc(x_splined, y_splined, point_num)
+    point_num = X_val.size * 100
+    x_splined, y_splined = x_inv, y_inv
+    # x_splined = y, y_splined = x
+    dx_dy = diff_calc(x_splined, y_splined) # x_splined(=等間隔ではない)をもとにしたdx_dy
     # 改めてスプライン補間によってX軸を揃える
-    x_splined_new, X_pdf_val_new = spline1(X_val, X_pdf_val, point_num)
-    assert (x_splined == x_splined_new).all()
-    Y_pdf_val = X_pdf_val_new * np.abs(dx_dy) # 要素ごとに掛ける
-    return x_splined, Y_pdf_val
+    start_ind, end_ind, X_pdf_val_new = spline_func(X_val, X_pdf_val, y_splined)
+    # plt_sca(x_splined[start_ind:end_ind], X_pdf_val_new, "after spline", x_splined[start_ind:end_ind], [laplace_func(np.log(x), 0) for x in x_splined[start_ind:end_ind]])
+    Y_pdf_val = X_pdf_val_new * np.abs(dx_dy[start_ind:end_ind]) # 要素ごとに掛ける
+    print("after transform, the prob", calc_prob(x_splined[start_ind:end_ind], Y_pdf_val))
+    correct_pdf = [(laplace_func(np.log(x), 1)/np.abs(x)).item() for x in x_splined[start_ind:end_ind]]
+    # plt_sca(x_splined[start_ind:end_ind], Y_pdf_val, "pdf of X_pdf_val_new", x_splined[start_ind:end_ind], [laplace_func(np.log(x), 0) for x in x_splined[start_ind:end_ind]])
+
+    # plt.scatter(x_splined[start_ind:end_ind][870:900], Y_pdf_val[870:900], color="green", s=0.2, label="estimated")
+    # plt.scatter(x_splined[start_ind:end_ind][870:900], correct_pdf[870:900], color="orange", s=0.2, label="correct")
+    # plt.title("pdf of new prob variable")
+    # plt.legend()
+    # plt.show()
+    # compressed_x, compressed_pdf = compress_range(x_splined[start_ind:end_ind], Y_pdf_val, th=0.99)
+    # plt.scatter(compressed_x, compressed_pdf, color="green", s=0.2, label="compressed")
+    # plt.title("compressed pdf")
+    # plt.legend()
+    # plt.show()
+    # TODO: 指定したデータ点だけスプライン補間を行う。ここでスプライン補間は行わずに畳み込みの時に行えば良いかも
+    # return spline_eq(compressed_x, compressed_pdf, 1000)
+    return x_splined[start_ind:end_ind], Y_pdf_val
+    # assert compressed_x.size == compressed_pdf.size
+    return compressed_x, compressed_pdf
+    return x, pdf
 
 """
 複数の確率変数に対して変数変換を行う
+X_vals: 配列の要素である確率変数の範囲を表す二次元配列
+X_pdf_vals: 配列の要素である確率密度関数の値の二次元配列
 """
 def trasform_vars(X_vals: np.ndarray, X_pdf_vals: np.ndarray, transform_func) -> tuple[np.ndarray, np.ndarray]:
     assert type(X_pdf_vals[0]) == np.ndarray or type(X_pdf_vals[0]) == list
@@ -57,9 +84,18 @@ def transform(input_data1: np.ndarray, input_data2: np.ndarray, transform_func, 
         sample_num = 1000
         y_sample_data1 = [np.random.laplace(data)  for data in input_data1 for _ in range(sample_num)]
         y_sample_data2 = [np.random.laplace(data)  for data in input_data2 for _ in range(sample_num)]
-        range_x = np.linspace(min(min(y_sample_data1), min(y_sample_data2))*10, max(max(y_sample_data1), max(y_sample_data2))*10, 1000)
+        range_x = np.linspace(min(min(y_sample_data1), min(y_sample_data2))*5, max(max(y_sample_data1), max(y_sample_data2))*5, 5000)
 
-        x, pdf1, pdf2 = transform_func(range_x, input_data1, input_data2)
-        return x, pdf1, pdf2
+        x1, x2, pdf1, pdf2 = transform_func(range_x, input_data1, input_data2)
+
+        if x1.size == x2.size and (x1 == x2).all():
+            return x1, pdf1, pdf2
+        else:
+            f1 = interpolate.interp1d(x1, pdf1, kind="cubic")
+            f2 = interpolate.interp1d(x2, pdf2, kind="cubic")
+            x_var = np.linspace(max(x1[0], x2[0]), min(x1[-1], x2[-1]), num=10000)
+            pdf1 = f1(x_var)
+            pdf2 = f2(x_var)
+            return x_var, pdf1, pdf2
     else:
         raise NotImplementedError
--- var-transform/transform_alg.py
+++ var-transform/transform_alg.py
@@ -3,6 +3,17 @@ from scipy import interpolate
 import matplotlib.pyplot as plt
 from transform import trasform_vars, trasform_var
 from noise_alg import laplace_func
+from utils import spline_eq, spline_func, calc_prob, compress_range, nonuniform_convolution, plt_sca
+
+def repeat_conv(val1, pdf1, val2, pdf2):
+    # 二つの確率密度関数のxの範囲を合わせる
+    while True:
+        conv_x, conv_pdf = np.convolve(pdf1, pdf2, mode='full')
+    
+        new_conv_x_range = val1[:min(val1.size, val2.size)] + val2[:min(val1.size, val2.size)]
+        start_ind, end_ind, new_pdf1 = spline_func(val1, pdf1, new_conv_x_range)
+        start_ind, end_ind, new_pdf2 = spline_func(val2, pdf2, new_conv_x_range)
+        return new_conv_x_range, new_pdf1, new_pdf2
 
 """
 Args:
@@ -24,49 +35,143 @@ def transform_sum(vals, pdf_vals):
     start_val = val1[0]
     dx = val1[1] - val1[0]
 
-    # 各PDFを順次畳み込み
-    for val2, pdf in zip(vals[1:], pdf_vals[1:]):
-        # TODO: ifとelseの処理が重複している
-        if dx == val2[1] - val2[0]:
-            result_pdf = np.convolve(result_pdf, pdf, mode='full') * dx
-            start_val = conv_x_range[0] + val2[0]
-            conv_x_size = conv_x_size + val2.size - 1
-            conv_x_range = np.arange(start_val, start_val + dx*conv_x_size, dx)
-            val1 = val2
-        else:
-            f2 = interpolate.interp1d(val2, pdf, kind="cubic")
-            # 最初の確率密度関数に合わせることになる
-            x2 = np.arange(val2[0], val2[-1], dx)
-            pdf2 = f2(x2)
+    if val1[2] - val1[1] == val1[1] - val1[0]:
+        # 各PDFを順次畳み込み
+        for val2, pdf in zip(vals[1:], pdf_vals[1:]):
+            # TODO: ifとelseの処理が重複している
+            if dx == val2[1] - val2[0]:
+                result_pdf = np.convolve(result_pdf, pdf, mode='full') * dx
+                start_val = conv_x_range[0] + val2[0]
+                conv_x_size = conv_x_size + val2.size - 1
+                conv_x_range = np.arange(start_val, start_val + dx*conv_x_size, dx)
+                val1 = val2
+            else:
+                f2 = interpolate.interp1d(val2, pdf, kind="cubic")
+                # 最初の確率密度関数に合わせることになる
+                x2 = np.arange(val2[0], val2[-1], dx)
+                pdf2 = f2(x2)
+                result_pdf = np.convolve(result_pdf, pdf2, mode='full') * dx
+                start_val = conv_x_range[0] + x2[0]
+                conv_x_size = conv_x_size + x2.size - 1
+                conv_x_range = np.arange(start_val, start_val + dx*conv_x_size, dx)
+                val1 = x2
+                print("conv_x_range: ", conv_x_range)
+                # TODO: conv_x_rangeとresult_pdfのサイズが異なる
+                plt_sca(conv_x_range, result_pdf, "convolution")
+        assert conv_x_range.size == result_pdf.size
+        return conv_x_range, result_pdf
+    # 一つの確率密度関数の横軸の確率変数が等間隔ではない場合
+    else:
+        # TODO: ここで確率が0.9以下の範囲になるように、確率変数を圧縮する。ここの圧縮が適切かどうかのFBを受けて再帰的に調節することも可能性としてある
+        # TODO: この圧縮は関数呼び出しの時にTrue/Falseを返すようにする
+        comp_val1, comp_result_pdf = compress_range(val1, result_pdf, th=0.9)
+        point_num = val1.size * 1 # TODO: ここはとりあえず適当に決めた
+        # conv_x_range, result_pdf = spline_eq(comp_val1, comp_result_pdf, point_num)
+        conv_x_size = conv_x_range.size
+        dx = conv_x_range[1] - conv_x_range[0]
+        result_pdf = comp_result_pdf
+        val1 = comp_val1
+        # 各PDFを順次畳み込み
+        for val2, pdf in zip(vals[1:], pdf_vals[1:]):
+            # FFTを使って畳み込みを行う
+            # val2, pdf = compress_range(val2, pdf, th=0.9)
+            print("start conv")
+            conv_result = nonuniform_convolution(val1, val2, result_pdf, pdf, val1)
+            print("end conv")
+            print("conv_prob: ", calc_prob(val1, conv_result))
+            result_pdf = conv_result
+            continue
+            # fig, axes = plt.subplots(1, 3, figsize=(10, 5))
+            # axes[0].scatter(comp_val1, comp_result_pdf, s=0.5, color="blue", label="before1")
+            # axes[1].scatter(val2, pdf, s=0.5, color="red", label="before2")
+            # axes[2].scatter(comp_val1, conv_result, s=0.5, color="green", label="after")
+            # plt.title("nonuniform convolution")
+            # plt.legend()
+            # plt.show()
+
+            # もう一方も同時に圧縮を行う
+            val2, pdf = compress_range(val2, pdf, th=0.9)
+            x2_range = np.arange(val2[0], val2[-1], dx)
+            # start_ind, end_indはそれぞれ0, x2_range.size-1を返すので不要
+            print("before_prob: ", calc_prob(val2, pdf))
+            start_ind, end_ind, pdf2 = spline_func(val2, pdf, x2_range)
+            x2_range = x2_range[start_ind:end_ind]
+            print("after_prob: ", calc_prob(x2_range, pdf2))
+            # fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+            # axes[0].scatter(val2, pdf, s=0.5, color="blue")
+            # axes[1].scatter(x2_range, pdf2, s=0.5, color="red")
+            # plt.show()
             result_pdf = np.convolve(result_pdf, pdf2, mode='full') * dx
-            start_val = conv_x_range[0] + x2[0]
-            conv_x_size = conv_x_size + x2.size - 1
+            start_val = conv_x_range[0] + x2_range[0]
+            conv_x_size = result_pdf.size
             conv_x_range = np.arange(start_val, start_val + dx*conv_x_size, dx)
-            val1 = x2
 
-    return conv_x_range, result_pdf
+            plt_sca(conv_x_range, result_pdf, "convolution")
 
-def transform_linear_sum(range_x, input_data1, input_data2):
+            # new_conv_x_range = val1[:min(val1.size, val2.size)] + val2[:min(val1.size, val2.size)]
+            # start_ind, end_ind, result_pdf = spline_func(conv_x_range, result_pdf, new_conv_x_range)
+            # conv_x_range = new_conv_x_range[start_ind:end_ind]
+            plt_sca(conv_x_range, result_pdf, "modify x in sum")
+        assert val1.size == result_pdf.size
+        return val1, result_pdf
+
+"""
+配列の確率変数にfuncという関数を適用した後に、合計を取るような関数の変換
+"""
+def transform_sum_after_func(range_x, input_data1, input_data2, func):
     # laplace_funを修正する
-    x_splined1, transformed_pdf1 = trasform_vars(range_x, laplace_func(range_x, loc=input_data1), lambda x: x)
-    x_splined2, transformed_pdf2 = trasform_vars(range_x, laplace_func(range_x, loc=input_data2), lambda x: x)
-
+    x_splined1, transformed_pdf1 = trasform_vars(range_x, laplace_func(range_x, loc=input_data1), func)
+    x_splined2, transformed_pdf2 = trasform_vars(range_x, laplace_func(range_x, loc=input_data2), func)
     # x_splined1/2はそれぞれ二次元配列
-    assert (x_splined1[0] == x_splined2[0]).all()
-    x_splined = x_splined1
+    # assert (x_splined1[0] == x_splined2[0]).all() # 必ずしもx軸を揃える必要はない
 
     # lambda x: x を適用後の確率密度関数を可視化する
-    for x_val, pdf1, pdf2 in zip(x_splined, transformed_pdf1, transformed_pdf2):
-        plt.scatter(x_val, pdf1, s=0.5, color="blue")
-        plt.scatter(x_val, pdf2, s=0.5, color="red")
+    for x_val1, x_val2, pdf1, pdf2 in zip(x_splined1, x_splined2, transformed_pdf1, transformed_pdf2):
+        plt.scatter(x_val1, pdf1, s=0.5, color="blue")
+        plt.scatter(x_val2, pdf2, s=0.5, color="red")
+    plt.title("after transform_func before sum")
     plt.show()
 
     if type(transformed_pdf1[0]) == np.ndarray:
-        sum_x1, sum_pdf1 = transform_sum(x_splined, transformed_pdf1)
-        sum_x2, sum_pdf2 = transform_sum(x_splined, transformed_pdf2)
-        assert (sum_x1 == sum_x2).all()
+        sum_x1, sum_pdf1 = transform_sum(x_splined1, transformed_pdf1)
+        sum_x2, sum_pdf2 = transform_sum(x_splined2, transformed_pdf2)
+        print("prob_x1: ", calc_prob(sum_x1, sum_pdf1))
+        print("prob_x2: ", calc_prob(sum_x2, sum_pdf2))
+        # assert (sum_x1 == sum_x2).all()
         sum_x = sum_x1
     else:
         pass
 
-    return sum_x, sum_pdf1, sum_pdf2
+    return sum_x1, sum_x2, sum_pdf1, sum_pdf2
+"""
+配列の要素に1をかけて合計をとる関数の変換
+"""
+def transform_linear_sum(range_x, input_data1, input_data2):
+    return transform_sum_after_func(range_x, input_data1, input_data2, lambda x: x)
+
+def transform_exp_beta_sum(range_x, input_data1, input_data2, beta):
+    return transform_sum_after_func(range_x, input_data1, input_data2, lambda x: np.exp(beta*x))
+
+"""
+ある単一の確率変数に対してlog関数を適用する変換
+"""
+def transform_log(range_x1, range_x2, pdf1, pdf2):
+    range_y1, pdf_y1 = trasform_var(range_x1, pdf1, lambda x: np.log(x))
+    range_y2, pdf_y2 = trasform_var(range_x2, pdf2, lambda x: np.log(x))
+
+    # x_splined1/2はそれぞれ二次元配列
+    plt_sca(range_y1, pdf_y1, title="after transform_log", x2=range_y2, y2=pdf_y2, s=0.5)
+    return range_y1, range_y2, pdf_y1, pdf_y2
+
+def transform_logsumexp(range_x, input_data1, input_data2):
+    # TODO: 畳み込みによりなぜかpdf1の値が負の値になる
+    x1, x2, pdf1, pdf2 = transform_exp_beta_sum(range_x, input_data1, input_data2, beta=1)
+    plt_sca(x1, pdf1, title="after transform_exp_beta_sum", x2=x2, y2=pdf2)
+    print("transform_logsumexp")
+    # TODO: 横軸の確率変数を統一する必要あり
+    if x1.size == x2.size and (x1 == x2).all():
+        x = x1
+    else:
+        print("x1 != x2")
+        x = x1
+    return transform_log(x1, x2, pdf1, pdf2)
