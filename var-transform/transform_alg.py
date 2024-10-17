fb66a7b617fc8b0b444ff8fd334de81c4c7cfbd1
TakumiHiraoka <hi.ta9.23.333@gmail.com>
2024-10-17 15:04:22 +0900

    add other transformation

--- var-transform/transform_alg.py
+++ var-transform/transform_alg.py
@@ -3,6 +3,7 @@ from scipy import interpolate
 import matplotlib.pyplot as plt
 from transform import trasform_vars, trasform_var
 from noise_alg import laplace_func
+from scipy import integrate
 from utils import spline_eq, spline_func, calc_prob, compress_range, nonuniform_convolution, plt_sca
 
 def transform_sum(vals, pdf_vals):
@@ -25,15 +26,21 @@ def transform_sum(vals, pdf_vals):
     start_val = val1[0]
     dx = val1[1] - val1[0]
 
+    print(val1[2] - val1[1])
+    print(val1[1] - val1[0])
     if val1[2] - val1[1] == val1[1] - val1[0]:
         # 各PDFを順次畳み込み
         for val2, pdf in zip(vals[1:], pdf_vals[1:]):
             # TODO: ifとelseの処理が重複している
             if dx == val2[1] - val2[0]:
+                print("start conv 1")
                 result_pdf = np.convolve(result_pdf, pdf, mode='full') * dx
                 start_val = conv_x_range[0] + val2[0]
                 conv_x_size = conv_x_size + val2.size - 1
+                print("size: ", len(result_pdf))
+                print("size: ", conv_x_size)
                 conv_x_range = np.arange(start_val, start_val + dx*conv_x_size, dx)
+                print("size(conv_x_range): ", len(conv_x_range))
                 val1 = val2
             else:
                 f2 = interpolate.interp1d(val2, pdf, kind="cubic")
@@ -115,16 +122,121 @@ def transform_log(range_x1, range_x2, pdf1, pdf2):
     range_y2, pdf_y2 = trasform_var(range_x2, pdf2, lambda x: np.log(x))
 
     # x_splined1/2はそれぞれ二次元配列
-    plt_sca(range_y1, pdf_y1, title="after transform_log", x2=range_y2, y2=pdf_y2, s=0.5)
+    plt_sca(range_y1, pdf_y1, title="after transform_log", x2=range_y2, y2=pdf_y2)
     return range_y1, range_y2, pdf_y1, pdf_y2
 
-def transform_logsumexp(range_x, input_data1, input_data2):
-    # TODO: 畳み込みによりなぜかpdf1の値が負の値になる
-    x1, x2, pdf1, pdf2 = transform_exp_beta_sum(range_x, input_data1, input_data2, beta=1)
-    plt_sca(x1, pdf1, title="after transform_exp_beta_sum", x2=x2, y2=pdf2)
-    return transform_log(x1, x2, pdf1, pdf2)
+def transform_exp(range_x1, range_x2, pdf1, pdf2):
+    """
+    ある単一の確率変数に対してlog関数を適用する変換
+    """
+    range_y1, pdf_y1 = trasform_var(range_x1, pdf1, lambda x: np.exp(x))
+    range_y2, pdf_y2 = trasform_var(range_x2, pdf2, lambda x: np.exp(x))
 
-def transform_linear_mul(range_x, pdf_x, a):
+    # x_splined1/2はそれぞれ二次元配列
+    plt_sca(range_y1, pdf_y1, title="after transform_exp", x2=range_y2, y2=pdf_y2)
+    return range_y1, range_y2, pdf_y1, pdf_y2
+
+def transform_logsumexp(range_x, input_data1, input_data2, beta=2.0):
+    x1, x2, pdf1, pdf2 = transform_exp_beta_sum(range_x, input_data1, input_data2, beta=beta)
+    plt_sca(x1, pdf1, title="after transform_exp_beta_sum", x2=x2, y2=pdf2)
+    x1_log, x2_log, pdf1_log, pdf2_log =  transform_log(x1, x2, pdf1, pdf2)
+    x1_result, pdf1_result = transform_scalar_mul(x1_log, pdf1_log, 1/beta)
+    x2_result, pdf2_result = transform_scalar_mul(x2_log, pdf2_log, 1/beta)
+    plt_sca(x1_result, pdf1_result, title="after transform_log", x2=x2_result, y2=pdf2_result)
+    return x1_result, x2_result, pdf1_result, pdf2_result
+
+def transform_laplace_exp(range_x, input_data):
+    x_list, pdf_list = [], []
+    for mu in input_data:
+        x, pdf = trasform_var(range_x, laplace_func(range_x, loc=mu), lambda x: np.exp(x))
+        x_list.append(x)
+        pdf_list.append(pdf)
+    return x_list, pdf_list
+
+def trasform_arg_max(range_x, input_data1, input_data2, beta=2.0):
+    """
+    配列の中の最大の要素を持つインデックスを返すアルゴリズム
+    """
+    print("start transform_arg_max")
+    sum_x1, sum_x2, sum_pdf1, sum_pdf2 = transform_exp_beta_sum(range_x, input_data1, input_data2, beta=1.0)
+    print("start transform_laplace_exp")
+    x1_list, pdf1_list = transform_laplace_exp(range_x, input_data1)
+    x2_list, pdf2_list = transform_laplace_exp(range_x, input_data2)
+    size_n = len(x1_list)
+    indices = np.linspace(0, 1, size_n)
+    x1_sm_list, pdf1_sm_list, x2_sm_list, pdf2_sm_list = [], [], [], []
+    x1_sm_list_all = [transform_scalar_mul(*transform_div(x, pdf, sum_x1, sum_pdf1), ind * (size_n-1)) for ind, x, pdf in zip(indices, x1_list, pdf1_list)]
+    x2_sm_list_all = [transform_scalar_mul(*transform_div(x, pdf, sum_x2, sum_pdf2), ind * (size_n-1)) for ind, x, pdf in zip(indices, x2_list, pdf2_list)]
+    print("start transform_sum")
+    for (x1_sm, pdf1_sm), (x2_sm, pdf2_sm) in zip(x1_sm_list_all, x2_sm_list_all):
+        x1_sm_list.append(x1_sm)
+        pdf1_sm_list.append(pdf1_sm)
+        x2_sm_list.append(x2_sm)
+        pdf2_sm_list.append(pdf2_sm)
+    print(x1_sm_list)
+    x1_argmax, pdf1_argmax = transform_sum(x1_sm_list, pdf1_sm_list)
+    x2_argmax, pdf2_argmax = transform_sum(x2_sm_list, pdf2_sm_list)
+    return x1_argmax, pdf1_argmax, x2_argmax, pdf2_argmax
+
+def transform_noisy_hist(range_x, input_data1, input_data2, beta=2.0):
+    """
+    ノイズ付きヒストグラムのアルゴリズム
+    """
+    x1, x2, pdf1, pdf2 = transform_exp_beta_sum(range_x, input_data1, input_data2, beta=beta)
+    plt_sca(x1, pdf1, title="after transform_exp_beta_sum", x2=x2, y2=pdf2)
+    x1_log, x2_log, pdf1_log, pdf2_log =  transform_log(x1, x2, pdf1, pdf2)
+    x1_result, pdf1_result = transform_scalar_mul(x1_log, pdf1_log, 1/beta)
+    x2_result, pdf2_result = transform_scalar_mul(x2_log, pdf2_log, 1/beta)
+    plt_sca(x1_result, pdf1_result, title="after transform_log", x2=x2_result, y2=pdf2_result)
+    return x1_result, x2_result, pdf1_result, pdf2_result
+
+def transform_mul(range_x, pdf_x, range_y, pdf_y, point_num=5000):
+    """
+    Z = XYの変換
+    """
+    # TODO: z = x * yの確率変数の範囲の探し方。基本的には端点の積が最大/最小になる
+    max_max_z = max(range_x) * max(range_y)
+    max_min_z = max(range_x) * min(range_y)
+    min_max_z = min(range_x) * max(range_y)
+    min_min_z = min(range_x) * min(range_y)
+    max_z = max(max_max_z, max_min_z, min_max_z, min_min_z)
+    min_z = min(max_max_z, max_min_z, min_max_z, min_min_z)
+    range_z = np.linspace(min_z, max_z, point_num)
+    f_interp = interpolate.CubicSpline(range_x, pdf_x, bc_type='natural', extrapolate=False)
+    g_interp = interpolate.CubicSpline(range_y, pdf_y, bc_type='natural', extrapolate=False)
+    pdf_z = []
+    min_range_x = min(range_x)
+    max_range_x = max(range_x)
+    for z in range_z:
+        integrand = lambda x: f_interp(z / x) * g_interp(x) / np.abs(x)
+        integral = integrate.quad(integrand, min_range_x, max_range_x, limit=100)[0]
+        pdf_z.append(integral)
+    return range_z, pdf_z
+
+def transform_div(range_x, pdf_x, range_y,  pdf_y, point_num=5000):
+    """
+    Z = X / Yの変換
+    """
+    # TODO: z = x / yの確率変数の範囲の探し方。必ずしも正負を考えると端点を考えれば良いわけではない。
+    max_max_z = max(range_x) / max(range_y)
+    max_min_z = max(range_x) / min(range_y)
+    min_max_z = min(range_x) / max(range_y)
+    min_min_z = min(range_x) / min(range_y)
+    max_z = max(max_max_z, max_min_z, min_max_z, min_min_z)
+    min_z = min(max_max_z, max_min_z, min_max_z, min_min_z)
+    range_z = np.linspace(min_z, max_z, point_num)
+    f_interp = interpolate.CubicSpline(range_x, pdf_x, bc_type='natural', extrapolate=False)
+    g_interp = interpolate.CubicSpline(range_y, pdf_y, bc_type='natural', extrapolate=False)
+    pdf_z = []
+    min_range_x = min(range_x)
+    max_range_x = max(range_x)
+    for z in range_z:
+        integrand = lambda x: f_interp(z * x) * g_interp(x) * np.abs(x)
+        integral = integrate.quad(integrand, min_range_x, max_range_x, limit=100)[0]
+        pdf_z.append(integral)
+    return range_z, pdf_z
+
+def transform_scalar_mul(range_x, pdf_x, a):
     """
     ある確率変数に対してaをかける変換
     """
@@ -132,7 +244,7 @@ def transform_linear_mul(range_x, pdf_x, a):
     pdf_y = pdf_x / a
     return range_y, pdf_y
 
-def transform_linear_add(range_x, pdf_x, a):
+def transform_scalar_add(range_x, pdf_x, a):
     """
     ある確率変数に対してaを足す変換
     """
