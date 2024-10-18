51b099247cda1447e8456e4cb1c3e0fe4f9918b8
TakumiHiraoka <hi.ta9.23.333@gmail.com>
2024-10-18 13:05:16 +0900

    delete var-transform

--- var-transform/noise_alg.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def laplace_func(x, loc, sensitivity=1, eps=0.1):
-    b = sensitivity / eps
-    if type(loc) == np.ndarray or type(loc) == list:
-        return [np.exp(-np.abs(x - val)/b) / (2*b) for val in loc]
-    return np.exp(-np.abs(x - loc)/b) / (2*b)
